cfaa26c6fe5badce92e75492ca58f75769c22703
Marcos Augusto <mjaojr@gmail.com>
2016-10-12 22:28:13 -0300

    (lab4): DateServer with java threads client handle

---
+++ lab4/DateServer.java
@@ -0,0 +1,81 @@
+/**
+ * Time-of-day server listening to port 6013.
+ *
+ */
+
+import java.net.*;
+import java.io.*;
+
+class ClientHanler implements Runnable {
+
+  private Socket con;
+  private InputStream in;
+  private InputStreamReader inr;
+  private BufferedReader bfr;
+
+  /**
+   * Contructor method
+   * @param  con type Socket
+   */
+  public ClientHanler (Socket con) {
+    this.con = con;
+    try {
+      in = con.getInputStream();
+      inr = new InputStreamReader(in);
+      bfr = new BufferedReader(inr);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Method run
+   */
+  public void run() {
+
+    try {
+
+      String msg;
+      OutputStream ou = this.con.getOutputStream();
+      OutputStreamWriter ouw = new OutputStreamWriter(ou);
+      BufferedWriter bfw = new BufferedWriter(ouw);
+      bfw.write("Seja bem-vindo! Iniciado em " + (new java.util.Date().toString()) + "\r\n");
+      bfw.flush();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+}
+
+public class DateServer
+{
+  private static Integer count = 0;
+
+	public static void main(String[] args) throws IOException {
+		Socket client = null;
+		ServerSocket sock = null;
+
+		try {
+			sock = new ServerSocket(6013);
+			// now listen for connections
+			while (true) {
+				client = sock.accept();
+        System.out.println("> Novo cliente! " + (++count) + " " + ((count==1)?"conexão iniciada":"conexões iniciadas"));
+				System.out.println("server = " + sock);
+				System.out.println("client = " + client);
+        Thread t = new Thread(new ClientHanler(client));
+        t.start();
+			}
+		}
+		catch (IOException ioe) {
+				System.err.println(ioe);
+		}
+		finally {
+			if (sock != null)
+				sock.close();
+			if (client != null)
+				client.close();
+		}
+	}
+}
